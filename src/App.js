dc3d77a3b2c66c78c213685a6479d487081f6572
Carmelo <gabrielnaut1@gmail.com>
2024-08-23 19:27:19 -0600

    añadiendo versel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CalculadoraImc from './components/CalculadoraImc';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <CalculadoraImc />
+      <SpeedInsights/>
     </div>
   );
 }
